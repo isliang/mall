0f16ddda7c54916d9354f6a9fa57f56b8f607a78
isliang <wslhdu@163.com>
2019-10-23 17:58:18 +0800

    update

---
+++ src/components/admin/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="api">
+        {{msg}}
+        <form id="login" action="http://192.168.56.101:1701/api/admin/user/login" method="post">
+            <p> <label for="name">Name</label>
+                <input
+                        id="name"
+                        v-model="name"
+                        type="text"
+                        name="name"
+                        placeholder="请输入用户名"
+                >
+            </p>
+            <p> <label for="password">Password</label>
+                <input
+                        id="password"
+                        v-model="password"
+                        type="password"
+                        name="password"
+                        placeholder="请输入密码"
+                >
+            </p>
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AdminLogin",
+        data() {
+            return {
+                msg: 'need login'
+            }
+        },
+        mounted () {
+            this.axios
+                .post('/api/product/detail?id=1&uid=1')
+                .then(response => (this.info = response.data.data))
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AdminLogin from '@/components/admin/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/admin/login',
+      name: 'AdminLogin',
+      component: AdminLogin
     }
   ]
 })
